71cdb50cf219d2e475ddce73b7545f14a6428eb5
chanellenewsome <chanellenewsome@yahoo.com>
2022-03-14 18:27:59 -0400

    final commits after deleting original files

---
+++ classes/Employee.js
@@ -0,0 +1,25 @@
+class Employee {
+    constructive (name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+
+getName() {
+    return  this.name;
+}
+getId() {
+    return this.id;
+}
+
+getEmail() {
+    return this.email;
+}
+
+getRole() {
+return "Employee";
+}
+
+}
+
+module.exports = Employee;
